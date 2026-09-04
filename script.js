e771e5015cdd9470008e403baab56c8d2b82b96d
omvishwase <omvishwase@gmail.com>
2026-09-04 16:16:51 +0530

    Finish the entire project. Add code for displaying score and winner

--- script.js
+++ script.js
@@ -15,36 +15,37 @@ function playRound(computerChoice, humanChoice) {
     if(humanChoice === "paper" && computerChoice === "rock") {
         const text = "You win! Paper beats Rock";
         addResultDiv(text);
-        console.log(text);
         humanScore++
+        displayScore(humanScore, computerScore);
     } else if(humanChoice === "rock" && computerChoice === "scissors") {
         const text = "You win! Rock beats Scissors";
         addResultDiv(text);
-        console.log(text);
         humanScore++
+        displayScore(humanScore, computerScore);
     } else if(humanChoice === "scissors" && computerChoice === "paper") {
         const text = "You win! Scissors beats Paper";
         addResultDiv(text);
-        console.log(text);
         humanScore++
+        displayScore(humanScore, computerScore);
     } else if(humanChoice === "paper" && computerChoice === "scissors") {
         const text = "You lose! Scissors beats Paper";
         addResultDiv(text);
-        console.log(text);
         computerScore++
+        displayScore(humanScore, computerScore);
     } else if(humanChoice === "rock" && computerChoice === "paper") {
         const text = "You lose! Paper beats Rock";
         addResultDiv(text);
-        console.log(text);
         computerScore++
+        displayScore(humanScore, computerScore);
     } else if(humanChoice === "scissors" && computerChoice === "rock") {
         const text = "You lose! Rock beats Scissors";
         addResultDiv(text);
-        console.log(text);
         computerScore++
+        displayScore(humanScore, computerScore);
     } else {
         const text = "It's a tie!";
         addResultDiv(text);
+        displayScore(humanScore, computerScore);
     }
 }
 
@@ -54,7 +55,7 @@ buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         let hChoice = e.target.id;
         let cChoice = getComputerChoice();
-        console.log(e.target.id)
+        // console.log(e.target.id)
         playRound(cChoice, hChoice);
     });
 });
@@ -71,4 +72,29 @@ function addResultDiv(text) {
     //appending code
     resultDiv.append(textSpan);
     buttonDiv.append(resultDiv);
+}
+
+function displayScore(humanScore, computerScore) {
+
+    const buttonDiv = document.getElementById("btn-div");
+    const scoreDiv = document.createElement("div");
+    scoreDiv.className = "score-div";
+
+    const textSpan = document.createElement("span");
+    textSpan.textContent = `Human Score: ${humanScore} | Computer Score: ${computerScore}`;
+
+    //appending code
+    scoreDiv.append(textSpan);
+    buttonDiv.append(scoreDiv);
+    const winnerText = humanScore === 5 ? "Human Wins!" :computerScore === 5 ? "Computer Wins!"  : null;
+
+    if(winnerText) {
+        const textDiv = document.createElement("h1");
+        textDiv.textContent = winnerText;
+        scoreDiv.append(textDiv);
+        const buttons = document.querySelectorAll("button");
+        buttons.forEach(button => {
+            button.remove();
+        })
+    }     
 }
