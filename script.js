771e765d8ff761762a4b65ebcb64e93173a3c86a
omvishwase <omvishwase@gmail.com>
2026-09-04 09:08:49 +0530

    Add the result div and update the playRound function

--- script.js
+++ script.js
@@ -2,40 +2,53 @@ let humanScore = 0, computerScore = 0;
 const values = ["rock", "paper", "scissors"]
 
 function getComputerChoice() {
-    const num = Math.floor(Math.random()*3)
-    return values[num]
+    const num = Math.floor(Math.random()*3);
+    return values[num];
 }
 
 function getHumanChoice() {
-    const humanChoice = prompt("Enter your choice")
-    return humanChoice
+    const humanChoice = prompt("Enter your choice");
+    return humanChoice;
 }
 
 function playRound(computerChoice, humanChoice) {
     if(humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You win! Paper beats Rock")
+        const text = "You win! Paper beats Rock";
+        addResultDiv(text);
+        console.log(text);
         humanScore++
     } else if(humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("You win! Rock beats Scissors")
+        const text = "You win! Rock beats Scissors";
+        addResultDiv(text);
+        console.log(text);
         humanScore++
     } else if(humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("You win! Scissors beat Paper")
+        const text = "You win! Scissors beats Paper";
+        addResultDiv(text);
+        console.log(text);
         humanScore++
     } else if(humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You lose! Scissors beats Paper")
+        const text = "You lose! Scissors beats Paper";
+        addResultDiv(text);
+        console.log(text);
         computerScore++
     } else if(humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You lose! Paper beat Rock")
+        const text = "You lose! Paper beats Rock";
+        addResultDiv(text);
+        console.log(text);
         computerScore++
     } else if(humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You lose! Rock beat Scissors")
+        const text = "You lose! Rock beats Scissors";
+        addResultDiv(text);
+        console.log(text);
         computerScore++
     } else {
-        console.log("It's a tie!")
+        const text = "It's a tie!";
+        addResultDiv(text);
     }
 }
 
-let buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
@@ -46,3 +59,16 @@ buttons.forEach(button => {
     });
 });
 
+function addResultDiv(text) {
+    const buttonDiv = document.getElementById("btn-div");
+    
+    const resultDiv = document.createElement("div");
+    resultDiv.className = "result-div";
+
+    const textSpan = document.createElement("span");
+    textSpan.textContent = text;
+    
+    //appending code
+    resultDiv.append(textSpan);
+    buttonDiv.append(resultDiv);
+}
